16410c30016821bc8db8c4bca3c7344abd258616
NguyenHung16 <hungvo03092@gmail.com>
2026-03-29 22:59:47 +0700

    feat: thêm Spring configs (Security, Hibernate, Thymeleaf...)

---
+++ BackEnd/src/main/java/com/vnh/configs/SpringSecurityConfigs.java
@@ -0,0 +1,141 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.vnh.configs;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import com.vnh.filters.JwtFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+/**
+ *
+ * @author Nguyen Hung
+ */
+@Configuration
+@EnableTransactionManagement
+@EnableWebSecurity
+@ComponentScan(basePackages = {
+    "com.vnh.controllers",
+    "com.vnh.repositories",
+    "com.vnh.services",
+    "com.vnh.filters"
+})
+public class SpringSecurityConfigs {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private JwtFilter jwtFilter;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtFilter jwtFilter() {
+        return new JwtFilter();
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher("/api/**", "/swagger-ui/**", "/v3/api-docs/**") // Thêm matcher cho Swagger
+                .csrf(csrf -> csrf.disable())
+                .cors(c -> c.configurationSource(corsConfigurationSource()))
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                // Cho phép truy cập giao diện và tài liệu Swagger
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**","/webjars/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/users", "/api/login").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/specialties/**", "/api/doctors/**", "/api/medicines/**").permitAll()
+                .requestMatchers("/api/patients/{patientId}/medical-records").authenticated()
+                .requestMatchers("/api/doctors/my-appointments").authenticated()
+                .requestMatchers(HttpMethod.POST, "/api/medicines").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/api/medicines/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/api/medicines/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(e -> e.authenticationEntryPoint((request, response, authException)
+                -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")));
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain webSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requests -> requests
+                .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
+                .requestMatchers("/", "/home").authenticated()
+                .requestMatchers("/medicines").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/login?error=true").permitAll())
+                .logout(logout -> logout.logoutSuccessUrl("/login").permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public HandlerMappingIntrospector mvcHandlerMappingIntrospector() {
+        return new HandlerMappingIntrospector();
+    }
+
+    @Bean
+    public Cloudinary cloudinary() {
+        Cloudinary cloudinary
+                = new Cloudinary(ObjectUtils.asMap(
+                        "cloud_name", "dr1dlhww5",
+                        "api_key", "996698978232863",
+                        "api_secret", "QsQHT5Zur10MvH1zIHhiVg28wBs",
+                        "secure", true));
+        return cloudinary;
+    }
+
+    @Bean
+    @Order(0)
+    public StandardServletMultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        config.setExposedHeaders(List.of("Authorization"));
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
