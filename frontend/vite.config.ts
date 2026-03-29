70eefca84f97ab1d4ea4adc9f9e141c71c73ea9f
Đăng Khoa <nldangkhoa0712@gmail.com>
2026-03-29 14:08:52 +0700

    config lib axios, tanstack, mui component

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,18 +5,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
+      // Chỉ cần dòng này là đủ để Vite hiểu @ là thư mục src
       '@': path.resolve(__dirname, './src'),
     },
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
+  server: {
+    port: 3000,
+    open: true,
+  },
 })
